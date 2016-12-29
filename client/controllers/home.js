96aedd642e2a4dfa97324f9f1aa304f05e2a9128
Yefry Alexis Calderon Yepes <alexiscalderon@MacBook-Pro-de-Yefry.local>
2016-12-28 21:48:40 -0500

    adding views and controllers

---
+++ client/controllers/home.js
@@ -0,0 +1,11 @@
+angular.module('Instagram')
+    .controller('HomeCtrl', function($scope){
+   
+        $scope.isAuthenticated = function(){
+            //check if is logged
+        };
+        
+        $scope.linkInstagram = function(){
+          //connect email account with instagram  
+        };
+});
